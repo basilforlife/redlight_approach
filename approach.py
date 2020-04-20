9433d38d288269124fe9aa978309ada84adfe8ed
Jonathan Roy <roy@alumni.mines.edu>
2020-04-19 23:22:22 -0600

    Added State class, flushed out some details of Approach class

--- approach.py
+++ approach.py
@@ -9,6 +9,13 @@ import numpy as np
 from math import floor, ceil
 
 
+# State class holds a vehicle state
+class State:
+    def __init__(self, pos, vel):
+        self.v = vel
+        self.x = pos
+
+
 # Approach Class implements the algorithm
 class Approach:
     def __init__(self):
@@ -16,37 +23,58 @@ class Approach:
         self.v_min = 0 # obvious since cars don't back up
         self.x_max = 0 # value of x at stoplight
 
-    # Set the initial conditions
-    def setInitConditions(self, x_start):
-        self.x_min = x_start # position at which vehicle starts at 
-
-    # Set the parameters that characterize the world
-    def setWorldParams(self, v_max, a_max):
-        self.v_max = v_max
-        self.a_max = a_max
-
     # Set the parameters used for computation
-    def setComputeParams(self, x_step, v_step, t_step):
+    def set_compute_params(self, x_step, v_step, t_step):
         self.x_step = x_step # discretization of position, in m
         self.v_step = v_step # discretization of velocity, in m/s
         self.t_step = t_step # length of time in seconds that a time step lasts
 
-    def stateToIdx(self, state):
+    # Set the parameters that characterize the world
+    def set_world_params(self, v_max, a_max):
 
-    def idxToState(self, idx):
+        # set v_max to the nearest smaller integer multiple of v_step s.t. 
+        self.v_max = floor(v_max / self.v_step) * self.v_step
+        self.a_max = a_max
 
+    # Set the initial conditions
+    # x_start should be negative
+    def set_init_conditions(self, x_start, v_start):
+        assert(x_start < 0), 'x_start should be negative'
+        self.x_min = round(x_start / self.x_step) * self.x_step # position at which vehicle starts at 
+        v_start_discrete = round(v_start / self.v_step) * self.v_step
+        self.initial_state = State(self.x_min, v_start_discrete)
+
+    # Compute the size of the state space
+    def compute_state_space_shape(self):
+        num_v_steps = int((self.v_max - self.v_min) / self.v_step) + 1
+        num_x_steps = int(abs(self.x_min - self.x_max) / self.x_step) + 1 
+        self.state_space_shape = (num_x_steps, num_v_steps)
+        self.state_space_flat_size = num_x_steps * num_x_steps
+
+    # Convert state object to index
+    def state_to_idx(self, state):
+
+        # This formula gives a bijection from state to index
+        idx = self.state_space_shape[1] * state.v + state.x
+        return idx
+
+    # Convert index to state object
+    def idx_to_state(self, idx):
+        x = idx % self.state_space_shape[1]
+        v = idx // self.state_space_shape[1]
+        return State(x, v)
 
     # This fn builds a state adjacency matrix
     # where the rows are states at t=k, and the columns are states at t=k+1
     # A True represents an edge
-    def buildAdjancencyMatrix(self):
-        self.state_size = # TODO SOME STATE SIZE FILL THIS IN -----------------------------------------------
+    def build_adjancency_matrix(self):
 
         # Init boolean array
-        self.adj_matrix = np.zeros((self.state_size, self.state_size), dtype=np.bool_) 
+        ss_flat_size = self.state_space_flat_size
+        self.adj_matrix = np.zeros((ss_flat_size, ss_flat_size), dtype=np.bool_) 
 
         # Iterate over all starting states
-        for i in range(self.state_size):
+        for i in range(ss_flat_size):
             state = self.idxToState(i)
             v_min, v_max = state.v - self.a_max, state.v + self.a_max # min, max of reachable states
             v_min_discrete = ceil(v_min / self.v_step) * self.v_step
@@ -56,9 +84,9 @@ class Approach:
             for v_new in np.arange(v_min_discrete, v_max_discrete, self.v_step): 
                 v_avg = (state.v + v_new)/2 # avg velocity over timestep
                 x_new = state.x + v_avg * self.t_step # change in position over timestep
-                new_state = State(x_new, v_new) # TODO MAKE STATE CLASS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                x_new_discrete = round(x_new / self.x_step) * x_step
+                new_state = State(x_new, v_new_discrete)
                 self.adj_matrix[i, stateToIdx(new_state)] = True # set appropriate edge to True
-
 
 
     # This fn gives the value of a state at a point in time
@@ -75,8 +103,8 @@ class Approach:
         
         return result
 
-    def findMaxNextState(self):
+    def find_max_next_state(self):
 
     def calcI(self):
 
-    def forwardPass(self):
+    def forward_pass(self):
