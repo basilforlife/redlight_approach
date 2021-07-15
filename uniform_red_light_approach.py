967eaee1766da2b32af0b079e08f7e634e7f35c0
Jonathan Roy <roy@alumni.mines.edu>
2021-07-15 15:02:23 -0600

    Head to head experiment simulation in uniform_red_light_approach.py. Also added pickling capability

--- uniform_red_light_approach.py
+++ uniform_red_light_approach.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import pickle
+from random import uniform
 import sys
 
 import traci
@@ -10,11 +12,24 @@ from Red_Light_Approach.state import State
 from Red_Light_Approach.sumo_utils import add_sumo_path
 
 
+# this fn gets random time and sets the light to red for that long
+def set_red_light_random():
+    red_duration = uniform(0, 10) + 20 # random value plus a constant 
+    traci.trafficlight.setPhase('2', 2) # set traffic light id=2 to red
+    traci.trafficlight.setPhaseDuration('2', red_duration)
+    print(f'Red light duration = {red_duration}')
+
+# This fn puts a vehicle in the left lane permanently
+def set_left_lane(vehicle_ID):
+    traci.vehicle.setLaneChangeMode(vehicle_ID, 0x000000000000)
+
 # Set up argparse
 parser = argparse.ArgumentParser(description='Run a SUMO simulation with an approach controller')
 parser.add_argument('path_to_sumocfg', type=str, help='relative or absolute path to .sumocfg file to run')
 parser.add_argument('path_to_approach_config', type=str, help='relative or absolute path to .json file to configure approach')
 parser.add_argument('-g', '--gui', action='store_true', help='Run the simulation in sumo-gui. Default is to run in sumo') 
+parser.add_argument('-p', '--pickle', action='store_true', help='Pickle approach object')
+parser.add_argument('-u', '--unpickle', action='store_true', help='Load approach object from pickle')
 args = parser.parse_args()
 
 # Ensure sumo is on system path
@@ -28,33 +43,62 @@ else:
 sumoCmd = [sumoBinary, '-c', args.path_to_sumocfg]
 
 
-# Set up approach stuff
-approach = Approach(args.path_to_approach_config)
-approach.build_adjacency_matrix()
-approach.backward_pass()
 
+# pickle approach bc its slow to build
+if args.pickle:
+    # Set up approach stuff
+    approach = Approach(args.path_to_approach_config)
+    approach.build_adjacency_matrix()
+    approach.backward_pass()
+
+    # pickle results
+    with open('approach.pickle', 'wb') as f:
+        pickle.dump(approach, f)
+
+# Unpickle if called for
+if args.unpickle:
+    with open('approach.pickle', 'rb') as f:
+        approach = pickle.load(f)
 
 # Enter traci context
 traci.start(sumoCmd)
 traci.vehicle.subscribe('vehicle_0', (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION, tc.VAR_SPEED, tc.VAR_NEXT_TLS))
+set_red_light_random()
+set_left_lane('vehicle_1')
 step = 0
 print(f'traci simulation step size: {traci.simulation.getDeltaT()}')
 begin_approach = False
-while step < 50:
+end_approach = False
+approach_timestep = 0
+sub_results = True # This gets overwritten with a dict, its empty when vehicle leaves sim
+while sub_results: # while vehicle is in simulation
     traci.simulationStep()
     print('step', step)
     sub_results = traci.vehicle.getSubscriptionResults('vehicle_0')
-    # TODO make state check boundaries to ensure state is within state space
-    state = State(sub_results[112][0][2] * -1, sub_results[64])
-    print(f'state = {state}')
+    if not sub_results:
+       break
+
+    # check that traffic light is ahead (that next_TLS result is full)
+    next_TLS = sub_results[112]
+    if next_TLS: # if traffic light ahead
+        # TODO make state check boundaries to ensure state is within state space
+        state = State(next_TLS[0][2] * -1, sub_results[64])
+        print(f'state = {state}')
+        green_light = (next_TLS[0][3] == 'G') # This is true if light is green
+        print(f'green_light = {green_light}')
 
     # Begin approach at 100m when we can "see" it
     if state.x >= -100 and not begin_approach:
         begin_approach = True
-        approach_timestep = 0
         traci.vehicle.setColor('vehicle_0', (246,186,34)) # Change color when approach starts
 
-    if begin_approach:
+    # End approach if it has started and the light is now green
+    if begin_approach and green_light:
+        end_approach = True
+        traci.vehicle.setSpeed('vehicle_0', -1) # Hand control back to sumo
+        traci.vehicle.setColor('vehicle_0', (24,255,0)) # Change color when approach ends
+
+    if begin_approach and not end_approach:
         next_state, _ = approach.forward_step(state, approach_timestep)
         traci.vehicle.setSpeed('vehicle_0', next_state.v)
         print(f'Approach modified speed: timestep = {approach_timestep}')
