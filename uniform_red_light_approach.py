817f32470c9a228762b9749e557a8b77323206cd
Jonathan Roy <roy@alumni.mines.edu>
2021-07-16 18:28:20 -0600

    vehicle type improvements, tripinfo reporting, gitignore updates

--- uniform_red_light_approach.py
+++ uniform_red_light_approach.py
@@ -14,13 +14,13 @@ from Red_Light_Approach.sumo_utils import add_sumo_path
 
 # this fn gets random time and sets the light to red for that long
 def set_red_light_random():
-    red_duration = uniform(0, 10) + 20 # random value plus a constant 
+    red_duration = uniform(0, 10) + 15.5 # random value plus a constant 
     traci.trafficlight.setPhase('2', 2) # set traffic light id=2 to red
     traci.trafficlight.setPhaseDuration('2', red_duration)
     print(f'Red light duration = {red_duration}')
 
 # This fn puts a vehicle in the left lane permanently
-def set_left_lane(vehicle_ID):
+def set_lane_change_static(vehicle_ID):
     traci.vehicle.setLaneChangeMode(vehicle_ID, 0x000000000000)
 
 # Set up argparse
@@ -43,7 +43,6 @@ else:
 sumoCmd = [sumoBinary, '-c', args.path_to_sumocfg]
 
 
-
 # pickle approach bc its slow to build
 if args.pickle:
     # Set up approach stuff
@@ -64,19 +63,17 @@ if args.unpickle:
 traci.start(sumoCmd)
 traci.vehicle.subscribe('vehicle_0', (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION, tc.VAR_SPEED, tc.VAR_NEXT_TLS))
 set_red_light_random()
-set_left_lane('vehicle_1')
+set_lane_change_static('vehicle_1')
+set_lane_change_static('vehicle_0')
 step = 0
 print(f'traci simulation step size: {traci.simulation.getDeltaT()}')
 begin_approach = False
 end_approach = False
 approach_timestep = 0
-sub_results = True # This gets overwritten with a dict, its empty when vehicle leaves sim
-while sub_results: # while vehicle is in simulation
+while step < 50:
     traci.simulationStep()
     print('step', step)
     sub_results = traci.vehicle.getSubscriptionResults('vehicle_0')
-    if not sub_results:
-       break
 
     # check that traffic light is ahead (that next_TLS result is full)
     next_TLS = sub_results[112]
