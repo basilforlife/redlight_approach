39ba9fb0d4a69608cdfd18c8ce1ead7022d81134
Jonathan Roy <roy@alumni.mines.edu>
2021-07-28 15:56:29 -0600

    approach_utils.py typehints and docstring

--- approach_utils.py
+++ approach_utils.py
@@ -1,13 +1,28 @@
 from math import ceil, floor
 
 
-# This fn rounds things to the nearest discrete step
-# TODO: this could take a fn as argument instead of behavior str
-def round_to_step(value, step_size, behavior="round"):
+def round_to_step(value: float, step_size: float, behavior: str = "round") -> float:
+    """Rounds a value to a multiple of `step_size`
+
+    Parameters
+    ----------
+    value
+        value to discretize
+    step_size
+        discrete unit to round to a multiple of
+    behavior
+        One of 'round', 'floor', and 'ceil'.
+
+    Returns
+    -------
+    float
+        The rounded value
+    """
+    int_scaled_value = value / step_size
     if behavior == "round":
-        result = round(value / step_size) * step_size
+        result = round(int_scaled_value) * step_size
     if behavior == "floor":
-        result = floor(value / step_size) * step_size
+        result = floor(int_scaled_value) * step_size
     if behavior == "ceil":
-        result = ceil(value / step_size) * step_size
+        result = ceil(int_scaled_value) * step_size
     return result
---
+++ tests/test_approach_utils.py
@@ -0,0 +1,14 @@
+from math import isclose
+
+from Red_Light_Approach.approach_utils import round_to_step
+
+
+class TestApproachUtils:
+    def test_round_to_step_default(self):
+        assert isclose(round_to_step(5.16, 0.1), 5.2)
+
+    def test_round_to_step_ceil(self):
+        assert isclose(round_to_step(5.14, 0.1, "ceil"), 5.2)
+
+    def test_round_to_step_floor(self):
+        assert isclose(round_to_step(5.16, 0.1, "floor"), 5.1)
