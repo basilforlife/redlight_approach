57f7403bb41b196e7ef2c8acb60b71f00ed4232f
Jonathan Roy <roy@alumni.mines.edu>
2021-08-03 15:57:49 -0600

    if __name__ == __main__ guards

--- red_light_approach.py
+++ red_light_approach.py
@@ -6,82 +6,83 @@ import numpy as np
 from Red_Light_Approach.sumo_simulation import SumoSimulation
 from Red_Light_Approach.sumo_utils import load_approach
 
-# Set up argparse
-parser = argparse.ArgumentParser(
-    description="Run a SUMO simulation with an approach controller"
-)
-parser.add_argument(
-    "-s",
-    "--sumocfg_file",
-    default="sumo/two_roads/f.sumocfg",
-    type=str,
-    help="path to .sumocfg file to run. Default sumo/two_roads/f.sumocfg",
-)
-parser.add_argument(
-    "-c",
-    "--approach_config_file",
-    type=str,
-    help="path to .json file to configure approach",
-)
-parser.add_argument(
-    "-p", "--pickle", type=str, help="File to pickle approach object to"
-)
-parser.add_argument(
-    "-u", "--unpickle", type=str, help="File to load approach object pickle from"
-)
-parser.add_argument(
-    "-g",
-    "--gui",
-    action="store_true",
-    help="Run the simulation in sumo-gui. Default is to run in sumo",
-)
-parser.add_argument(
-    "-v",
-    "--verbose",
-    action="store_true",
-    help="Print information about each timestep to console",
-)
-parser.add_argument(
-    "-N",
-    default=1,
-    type=int,
-    help="This option runs the simulation N times and if N > 1, it plots them. Defaults to N=1.",
-)
-args = parser.parse_args()
+if __name__ == "__main__":
+    # Set up argparse
+    parser = argparse.ArgumentParser(
+        description="Run a SUMO simulation with an approach controller"
+    )
+    parser.add_argument(
+        "-s",
+        "--sumocfg_file",
+        default="sumo/two_roads/f.sumocfg",
+        type=str,
+        help="path to .sumocfg file to run. Default sumo/two_roads/f.sumocfg",
+    )
+    parser.add_argument(
+        "-c",
+        "--approach_config_file",
+        type=str,
+        help="path to .json file to configure approach",
+    )
+    parser.add_argument(
+        "-p", "--pickle", type=str, help="File to pickle approach object to"
+    )
+    parser.add_argument(
+        "-u", "--unpickle", type=str, help="File to load approach object pickle from"
+    )
+    parser.add_argument(
+        "-g",
+        "--gui",
+        action="store_true",
+        help="Run the simulation in sumo-gui. Default is to run in sumo",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Print information about each timestep to console",
+    )
+    parser.add_argument(
+        "-N",
+        default=1,
+        type=int,
+        help="This option runs the simulation N times and if N > 1, it plots them. Defaults to N=1.",
+    )
+    args = parser.parse_args()
 
-# Load approach object according to command line args
-approach = load_approach(args)
+    # Load approach object according to command line args
+    approach = load_approach(args)
 
-# Configure some filenames
-route_filename = "sumo/two_roads/f.rou.xml"
-first_edge_len = 200
+    # Configure some filenames
+    route_filename = "sumo/two_roads/f.rou.xml"
+    first_edge_len = 200
 
-# Configure SumoSimulation
-sumo_sim = SumoSimulation(
-    approach,
-    args.sumocfg_file,
-    route_filename,
-    first_edge_len,
-    args.gui,
-    args.verbose,
-)
+    # Configure SumoSimulation
+    sumo_sim = SumoSimulation(
+        approach,
+        args.sumocfg_file,
+        route_filename,
+        first_edge_len,
+        args.gui,
+        args.verbose,
+    )
 
-# Set up simulation stuff
-num_samples = args.N
-red_durations = approach.green_distribution.sample(
-    num_samples
-)  # random sample from uniform distribution
+    # Set up simulation stuff
+    num_samples = args.N
+    red_durations = approach.green_distribution.sample(
+        num_samples
+    )  # random sample from uniform distribution
 
-# Run simulation N times
-timeloss = [sumo_sim.run(red_duration) for red_duration in red_durations]
+    # Run simulation N times
+    timeloss = [sumo_sim.run(red_duration) for red_duration in red_durations]
 
-# Plot
-if args.N > 1:
-    plt.scatter(red_durations, timeloss)
-    plt.title("test")
-    plt.show()
+    # Plot
+    if args.N > 1:
+        plt.scatter(red_durations, timeloss)
+        plt.title("test")
+        plt.show()
 
-# Print avg time saved
-if args.N == 1:
-    print(f"Red light duration: {red_durations[0]}")
-print(f"Mean time saved: {np.mean(timeloss):.2f}s")
+    # Print avg time saved
+    if args.N == 1:
+        print(f"Red light duration: {red_durations[0]}")
+    print(f"Mean time saved: {np.mean(timeloss):.2f}s")
--- run_approach.py
+++ run_approach.py
@@ -5,35 +5,35 @@ import numpy as np
 
 from Red_Light_Approach.approach import Approach
 
-# Load test case parameters
-json_path = "tests/test_params.json"
+if __name__ == "__main__":
+    # Load test case parameters
+    json_path = "tests/test_params.json"
 
-approach = Approach()
-approach.configure(json_path)
-approach.build_adjacency_matrix()
-approach.backward_pass()
-approach.forward_pass()
+    approach = Approach()
+    approach.configure(json_path)
+    approach.build_adjacency_matrix()
+    approach.backward_pass()
+    approach.forward_pass()
 
+    # Set up layer of adj matrix to plot
+    M = approach.adj_matrix
+    ss_size = approach.state_space_shape
+    s = approach.initial_state
 
-# Set up layer of adj matrix to plot
-M = approach.adj_matrix
-ss_size = approach.state_space_shape
-s = approach.initial_state
+    # Plot how many states are reachable
+    if False:
+        reachable_sum = [
+            M[x, v].sum() for x, v in product(range(ss_size[0]), range(ss_size[1]))
+        ]
+        reachable_sum = np.reshape(reachable_sum, ss_size)
+        i, j = approach.state_to_indices(s)
+        reachable_sum[i, j] = 7
+        plt.imshow(reachable_sum, cmap="gray")
+        plt.colorbar()
+        plt.show()
 
-# Plot how many states are reachable
-if False:
-    reachable_sum = [
-        M[x, v].sum() for x, v in product(range(ss_size[0]), range(ss_size[1]))
-    ]
-    reachable_sum = np.reshape(reachable_sum, ss_size)
-    i, j = approach.state_to_indices(s)
-    reachable_sum[i, j] = 7
-    plt.imshow(reachable_sum, cmap="gray")
-    plt.colorbar()
-    plt.show()
-
-# Plot reachable states from one state
-if False:
-    M[i, j][i, j] = True
-    plt.imshow(M[i, j])
-    plt.show()
+    # Plot reachable states from one state
+    if False:
+        M[i, j][i, j] = True
+        plt.imshow(M[i, j])
+        plt.show()
